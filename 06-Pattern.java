535fa32d13bb94575b8425bdad8732dde5d7dea4
Tarun Kumar Pandey <128689278+Tarun727272@users.noreply.github.com>
2023-09-09 16:36:06 +0530

    Create 06-Pattern.java

---
+++ 06-Pattern.java
@@ -0,0 +1,27 @@
+/*      _____##### *  *  *  *  *
+        _____##### *  *  *  *  *
+        _____##### *  *  *  *  *
+        _____##### *  *  *  *  *
+        _____##### *  *  *  *  *
+
+  */
+
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                System.out.print("_");
+
+            }
+            for (int j=1;j<=5;j++){
+                System.out.print("#");
+
+            }
+            for (int j=1;j<=5;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
