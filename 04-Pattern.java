ca4f7126774edd155a93d64e4cd3b33151ed6fe9
Tarun Kumar Pandey <128689278+Tarun727272@users.noreply.github.com>
2023-09-09 16:33:17 +0530

    Create 04-Pattern.java

---
+++ 04-Pattern.java
@@ -0,0 +1,21 @@
+/* 04
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+       for (int i=1;i<=5;i++){
+           for (int j=1;j<=6-i;j++){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+    }
+}
